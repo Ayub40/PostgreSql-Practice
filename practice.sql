c91111c8d81c48a84eec3d1c871ac96c7de88f9c
Ayub40 <ayubk4028@gmail.com>
2025-05-22 01:32:55 +0600

    added query practice

--- practice.sql
+++ practice.sql
@@ -250,3 +250,66 @@ JOIN departments USING(department_id)
 GROUP BY department_name
 ORDER BY avg_salary DESC
 LIMIT 1;
+
+-- 5. Count Employees Hired Each Year
+SELECT extract(YEAR FROM hire_date) as hire_year, count(*) FROM employees
+GROUP BY hire_year;
+
+-- ================================================================================
+-- ================================================================================
+-- ================================================================================
+
+-- 10-1 Query Practice: Part 3
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10, 2)
+);
+
+-- Inserting sample data into the orders table
+INSERT INTO orders (customer_id, order_date, total_amount) VALUES 
+    (1, '2022-01-05', 100.50),
+    (2, '2022-01-07', 200.75),
+    (1, '2022-01-08', 150.25),
+    (3, '2022-01-10', 300.00),
+    (2, '2022-01-15', 180.50),
+    (3, '2022-01-20', 220.25),
+    (1, '2022-01-25', 90.00),
+    (2, '2022-01-28', 120.75),
+    (3, '2022-02-01', 250.50),
+    (1, '2022-02-05', 180.25),
+    (4, '2023-02-05', 190.25);
+
+DROP TABLE orders;
+
+-- Find customers who have placed more than 2 orders and calculate the total amount spent by each of these customers.
+
+SELECT * FROM orders;
+
+    -- SELECT customer_id, count(order_id) from orders GROUP BY customer_id HAVING count(order_id) > 2;
+
+    SELECT customer_id, count(order_id), sum(total_amount) as total_spent from orders GROUP BY customer_id HAVING count(order_id) > 2;
+
+--   Find the total amount of orders placed each month in the year 2022.
+
+-- SELECT extract(MONTH FROM order_date) FROM orders;
+-- SELECT extract(month FROM order_date) FROM orders;
+
+SELECT extract(month from order_date) as month, sum(total_amount)  from orders WHERE extract(year from order_date) = 2022 GROUP BY month;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
